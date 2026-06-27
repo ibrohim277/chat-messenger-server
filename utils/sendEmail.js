15671cf3fec7dbbb2a066bb639c36f0128e13e38
ibrohim277 <farhodsadullayev2@gmail.com>
2026-06-27 16:52:35 +0500

    fix smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,34 +6,35 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
 
-host:"smtp.gmail.com",
+  host: "smtp.gmail.com",
 
-port:465,
+  port: 465,
 
-secure:true,
+  secure: true,
 
-auth:{
-user:process.env.EMAIL_USER,
-pass:process.env.EMAIL_PASS
-}
-
-});
-
-
-export const sendCode = async(email,code)=>{
-
-
-await transporter.sendMail({
-
-from:process.env.EMAIL_USER,
-
-to:email,
-
-subject:"Password reset code",
-
-text:`Your code: ${code}`
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 
 });
+
+
+
+export const sendCode = async (email, code) => {
+
+
+  await transporter.sendMail({
+
+    from: process.env.EMAIL_USER,
+
+    to: email,
+
+    subject: "Password reset code",
+
+    text: `Your code: ${code}`
+
+  });
 
 
 };
